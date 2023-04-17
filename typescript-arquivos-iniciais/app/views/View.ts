f7e68680727fd5889d64f04a87ea8bf098e5806a
aoc <michaelmoranesul@gmail.com>
2023-04-16 23:25:29 -0300

    heranca com typescript, classes abstratas, modificador protected, tipos genericos em classes

--- typescript-arquivos-iniciais/app/views/View.ts
+++ typescript-arquivos-iniciais/app/views/View.ts
@@ -1,4 +1,4 @@
-export class View<T> {
+export abstract class View<T> {
     protected elemento: HTMLElement;
 
     constructor(seletor: string) {
@@ -8,7 +8,5 @@ export class View<T> {
         const template = this.template(model);
         this.elemento.innerHTML = template;
     }
-    template(model: T): string {
-        throw Error("Classe filha precisa implementar o metodo template")
-    }
+    abstract template(model: T): string;
 }
